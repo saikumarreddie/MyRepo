90a13c701780f6054abb19d781baa9b3598d6155
saikumarreddie <saikumarreddybandi6@gmail.com>
2021-02-28 07:47:49 +0530

    my next commit

--- Sample.java
+++ Sample.java
@@ -2,7 +2,7 @@ class Main{
 
 public static void main(String []args){
 
-System.out.println("hello world");
+System.out.println("welcome to world of GIT");
 
 }
 }
---
+++ test.java
@@ -0,0 +1,8 @@
+class Main1{
+
+public static void main(String []args){
+
+System.out.println("hello main1");
+
+}
+}
